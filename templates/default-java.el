0db41ae5b752293bd426f9966001989f515437d2
Ivan Yonchovski <yyoncho@gmail.com>
2023-05-25 21:30:03 +0300

    WIp

--- templates/default-java.el
+++
@@ -1,7 +0,0 @@
-package `(mapconcat 'identity (split-string (replace-regexp-in-string ".*src\\(/\\(main\\|test\\)\\)?\\(/java\\)?" "" default-directory) "/" t) ".")`;
-
-public class `(file-name-base
-               (or (buffer-file-name)
-                   (buffer-name)))` {
-$0
-}
